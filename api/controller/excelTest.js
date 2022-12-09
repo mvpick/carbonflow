de38592bbf450f59c8ed8059b2e57cf8aa0abec4
아니양 <byhj0309@gmail.com>
2022-12-09 12:14:11 +0900

    json

--- api/controller/excelTest.js
+++ api/controller/excelTest.js
@@ -1,10 +1,14 @@
-import excelToJson from 'convert-excel-to-json'
+import { assert } from 'console';
+import excelToJson from 'convert-excel-to-json';
+import fs from 'fs';
+import excelData from '../../json/excelData.json';
 
 export const upload = async (req, res) => {
   try {
-    const file = req.file;
+    const file = req.file; // 어드민이 업로드한 파일
+    const selectedYear = req.body.selectedYear; // 어드민이 선택한 연도
 
-    const result = excelToJson({
+    const result = excelToJson({ // 엑셀 파일 읽어들이기
       sourceFile: file.path,
       sheets: [
         {
@@ -33,18 +37,19 @@ export const upload = async (req, res) => {
       ],
     });
 
-    const createdArray = Object.keys(result)
+    excelData[selectedYear] = result; // 해당 연도에 읽어들인 데이터 주입 (없으면 생성, 있으면 덮어쓰기)
 
-    for(let keyItem of createdArray) {
-      for(let item of result[keyItem]) {
-        console.log(item)
-      }
-    }
+    const convertedJSON = JSON.stringify(excelData); // 엑셀 파일을 JSON형식으로 변환
+
+    fs.writeFileSync('json/excelData.json', convertedJSON, (err) => { // JSON파일 생성
+      console.error(err);
+    })
 
     return res.status(200).send({
       message: 'success'
     })
   } catch(err) {
+    console.error(err);
     return res.status(400).send({
       message: 'fail'
     })
