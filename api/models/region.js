df6f9cef3cba81d11169eeeee63aae5fdd65ebbf
아니양 <byhj0309@gmail.com>
2022-12-13 17:07:54 +0900

    chartdata

--- api/models/region.js
+++ api/models/region.js
@@ -1,26 +1,25 @@
-const Sequelize = require('sequelize');
-
+'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const region = sequelize.define('region', {
-    idx : {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false
-    },
-    name : {
-      type: DataTypes.STRING,
-      allowNull: false,
-      comment: '지역 이름'
-    },
-  }, {
-    tableName: 'region',
-  })
-  // region.associate = function(models){
-  //   region.belongsTo(models.lecture, {
-  //         foreignKey: 'lecture_idx',
-  //         onDelete: 'CASCADE',
-  //     })
-  // }
-  return region
+    const region = sequelize.define('region', {
+        idx : {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        name : {
+            type: DataTypes.STRING,
+            allowNull: false,
+            comment: '지역명'
+        }
+    }, {
+        tableName: 'region',
+    })
+    region.associate = function(models){
+        // file.belongsTo(models.school, {
+        //     foreignKey: 'school_idx',
+        //     onDelete: 'CASCADE',
+        // })
+    }
+    return region
 }
--- pages/chart.vue
+++ pages/chart.vue
@@ -3,28 +3,87 @@
     <div class="com_contain">
         <section class="tab_section">
             <div class="flex_box">
-                <button @click="on_tab=0" 
+                <button @click="on_tab=0"
                 :class="{ 'on_tab' : on_tab === 0 }">산업별 탄소배출량</button>
                 <button @click="on_tab=1"
                 :class="{ 'on_tab' : on_tab === 1 }">연도별 탄소배출량</button>
             </div>
         </section>
-        
-        <div class="industry" v-if="on_tab==0">
 
+        <div class="industry" v-if="on_tab==0">
         </div>
         <div class="year" v-if="on_tab==1">
-            
+          <BarChart :data="barChartData" :options="barChartOptions" />
         </div>
     </div>
 </div>
 </template>
 
-<script>    
+<script>
 export default{
 data() {
-    return{
-        on_tab:0,
+  return{
+      on_tab:0,
+      barChartData: {
+        labels: [
+          "2019-06",
+          "2019-07",
+          "2019-08",
+          "2019-09",
+          "2019-10",
+          "2019-11",
+          "2019-12",
+          "2020-01",
+          "2020-02",
+          "2020-03",
+        ],
+        datasets: [
+          {
+            label: "Visualizaciones",
+            data: [2, 1, 16, 3, 4, 5, 0, 0, 4, 12, 2],
+            backgroundColor: "rgba(20, 255, 0, 0.3)",
+            borderColor: "rgba(100, 255, 0, 1)",
+            borderWidth: 2,
+          },
+        ],
+      },
+      barChartOptions: {
+        responsive: true,
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: "Google analytics data",
+          fontSize: 24,
+          fontColor: "#6b7280",
+        },
+        tooltips: {
+          backgroundColor: "#17BF62",
+        },
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                max: 7,
+                min: 0,
+                stepSize: 1,
+              },
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+        },
+      },
     }
   }
 }
