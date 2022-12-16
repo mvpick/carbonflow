4920495bdb20908127bde373680a3d099b280623
dmstlf292 <silviapark292@gmail.com>
2022-12-16 15:48:12 +0900

    chart

--- api/controller/allData.js
+++ api/controller/allData.js
@@ -1,14 +1,20 @@
 const YearEmissions = require('../db/models').yearEmissions;
 const IndustryEmissions = require('../db/models').industryEmissions;
 const Year = require('../db/models').year;
+const sequelize = require("../db/models").sequelize;
 
 export const getAllData = async(req, res) => {
     try {
-        
-        const year_info = await YearEmissions.findAll({ order: [['created_at', 'DESC']] });
-        const industry_info = await IndustryEmissions.findAll({ order: [['created_at', 'DESC']] });
-        const year = await Year.findAll({ attributes: ['id', 'name'], });
-        return res.status(200).send({ code: 200, message: '모든 차트 데이터 조회 성공', data : [industry_info, year_info, year] })
+        const industry_info = await IndustryEmissions.findAll({ order: [['created_at', 'DESC']] }); // 부문별
+        const year_info = await YearEmissions.findAll({ 
+            group: ['value'],
+            order: [[sequelize.fn('max', sequelize.col('value')), 'DESC']],
+            raw: true
+        }); // 연도별
+        const year = await Year.findAll({ attributes: ['id', 'name'], order: [['name', 'DESC']] }); // 조회 연도
+
+
+        return res.status(200).send({ code: 200, message: '모든 차트 데이터 조회 성공', data : {industry_info, year_info, year} })
         
     } catch (error) {
         console.log(error,'error')
--- pages/chart.vue
+++ pages/chart.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="chart">
     <div class="com_contain">
-        <select name="" id="" v-model="selected_year_id" style="background-color:red">
+        <select :disabled="on_tab === 1" name="" id="" v-model="selected_year_id" style="background-color:red">
           <option :value="item.id" v-for="(item, index) in year_select" :key="index">
               {{item.name}}
           </option>
@@ -38,115 +38,43 @@ export default{
       year_select : [],
       barChartData : null,
       barChartOptions : null,
-      
-
-      // barChartData: {
-      //   labels: ["2019-06", "2019-07", "2019-08", "2019-09", "2019-10", "2019-11"],
-      //   datasets: [
-      //     {
-      //       label: "Visualizaciones",
-      //       data: [2, 1, 16, 3, 4, 5],
-      //       backgroundColor: "rgba(20, 255, 0, 0.3)",
-      //       borderColor: "rgba(100, 255, 0, 1)",
-      //       borderWidth: 2,
-      //     },
-      //   ],
-      // },
-      // barChartOptions: {
-      //   responsive: true,
-      //   legend: {
-      //     display: false,
-      //   },
-      //   title: {
-      //     display: false
-      //   },
-      //   tooltips: {
-      //     backgroundColor: "#17BF62",
-      //   },
-      //   scales: {
-      //     xAxes: [
-      //       {
-      //         gridLines: {
-      //           display: true,
-      //         },
-      //       },
-      //     ],
-      //     yAxes: [
-      //       {
-      //         ticks: {
-      //           beginAtZero: true,
-      //           max: 7,
-      //           min: 0,
-      //           stepSize: 1,
-      //         },
-      //         gridLines: {
-      //           display: true,
-      //         },
-      //       },
-      //     ],
-      //   },
-      // },
+      ticks_max : null,
     }
   },
   mounted(){
-    this.show_table('pieChartData', 0);
     this.getDatas();
+    this.show_table('pieChartData', 0);
   },
   watch:{
     "selected_year_id"(){
-      
       // 부문별 탄소 배출량
-      if(this.on_tab === 0 && !!this.selected_year_id){
-        console.log('부문별 탄소 배출량 실행')
+      if(this.on_tab === 0 && this.selected_year_id !== null){
+        console.log('부문별 탄소 배출량 탭')
         this.industry_info = this.industry_info.filter(item=>{
-          return item.id === this.selected_year_id;
+          return this.selected_year_id === item.id;
         });
-        console.log(this.industry_info,'this.industry_info')
-
-        this.pieChartData = {
-          labels: ['에너지', '산업공정', '농업', 'LULUCF', '폐기물'],
-          datasets: [
-            {
-              label: 'Dataset 1',
-              data: [this.industry_info.energy ,this.industry_info.process , this.industry_info.agriculture , this.industry_info.lulucf, this.industry_info.waste],
-              backgroundColor: ['red', 'blue', 'green', 'black', 'yellow'],
-            }
-          ]
-        };
-        console.log(this.pieChartData,'this.pieChartData')
-        this.pieChartOptions = {
-          responsive: true,
-          plugins: {
-            legend: {
-              position: 'top',
-            },
-            title: {
-              display: false
-            }
-          }
-        }
-
+        console.log(this.industry_info,'this.industry_info 전체전체')
+        this.show_table('pieChartData', 0, this.industry_info);
       }
-
+     
+    },
+    "on_tab"(){
       // 연도별 탄소 배출량
-      if(this.on_tab === 1 && !!this.selected_year_id){
-        this.year_info = this.year_info.filter(item=>{
-          return item.id === this.selected_year_id;
-        });
+      if(this.on_tab === 1){
+        this.show_table('barChartOptions', 1, this.year_info);
       }
-
-
     }
   },
   methods:{
     getDatas(){
       this.$axios.get('/allData/getAllData')
       .then(res => {
-        this.year_select = res.data.data[2];
+        this.year_select = res.data.data.year;
+        this.ticks_max = Math.round(res.data.data.year_info[0].value) ;
 
         // 부문별 탄소 배출량
-        for(let one of res.data.data[0]){
-          let [__] = res.data.data[2].filter(item=>{
+        for(let one of res.data.data.industry_info){
+          let [__] = res.data.data.year.filter(item=>{
             return one.year_id === item.id;
           })
           one.year = __
@@ -154,52 +82,121 @@ export default{
         }
 
         // 연도별 탄소 배출량
-        for(let one of res.data.data[1]){
-          let [__] = res.data.data[2].filter(item=>{
-            return one.year_id === item.id;
+        for(let one of res.data.data.year){
+          let [__] = res.data.data.year_info.filter(item=>{
+            return one.id === item.year_id;
           })
-          one.year = __
-          this.year_info.push(one)
+
+          if(!!__) { 
+            one.year = __;
+            this.year_info.push(one);
+          };
+          
         }
 
       }).catch(err => {
         console.log(err);
       })
     },
-    show_table(type, index){
-      console.log(type,' <<< type ??')
-      console.log(index,'<<< index??')
-
-
+    show_table(type, index, data){
       this.on_tab = index;
       if(type==='pieChartData' && index === 0){
-        console.log(this.industry_info,'선택된 부문별 탄소 배출량')
-
-        this.pieChartData = {
-          labels: ['에너지', '산업공정', '농업', 'LULUCF', '폐기물'],
-          datasets: [
-            {
-              // label: 'Dataset 1',
-              data: [100,200,-300,400, 500],
-              backgroundColor: ['red', 'blue', 'green', 'black', 'yellow'],
-            }
-          ]
-        };
-        this.pieChartOptions = {
-          responsive: true,
-          plugins: {
-            legend: {
-              position: 'top',
-            },
-            title: {
-              display: false
+        console.log('선택된 부문별 탄소 배출량 : ' , data);
+        if(!!data){
+          this.pieChartData.datasets[0].data = [];  // 데이터 수치 리셋
+          this.pieChartData.datasets[0].data = [data[0].energy ,data[0].process , data[0].agriculture , data[0].lulucf, data[0].waste];
+        }
+        else{
+          this.pieChartData = {
+            labels: ['에너지', '산업공정', '농업', 'LULUCF', '폐기물'],
+            datasets: [
+              {
+                data: [100,200,-300,400, 500],
+                backgroundColor: ['red', 'blue', 'green', 'black', 'yellow'],
+              }
+            ]
+          };
+          this.pieChartOptions = {
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'top',
+              },
+              title: {
+                display: false
+              }
             }
           }
         }
       }
       
-      if(type==='barChartOptions'){
-        console.log(this.industry_info,'선택된 연도별 탄소 배출량')
+      if(type==='barChartOptions' && index === 1){
+        console.log('전체 연도별 탄소 배출량 : ' , data);
+
+        if(!!data){
+          let x_year = [];
+          let y_data = [];
+          for(let one of data){
+            x_year.push(one.name);
+            y_data.push(one.year.value);
+          };
+
+          this.barChartOptions.scales.yAxes[0].ticks.max = 0; // y축 최대값 수치 리셋
+          this.barChartData.labels = [];  // 데이터 기간 리셋
+          this.barChartData.datasets[0].data = [];  // 데이터 수치 리셋
+
+          this.barChartOptions.scales.yAxes[0].ticks.max = this.ticks_max;
+          this.barChartData.labels = x_year;
+          this.barChartData.datasets[0].data = y_data;
+        }else{
+          this.barChartData = {
+            labels: ["2019-06", "2019-07", "2019-08", "2019-09", "2019-10", "2019-11"], // x 축
+            datasets: [
+              {
+                label: "Visualizaciones",
+                data: [2, 1, 16, 3, 4, 5], // y축
+                backgroundColor: "rgba(20, 255, 0, 0.3)",
+                borderColor: "rgba(100, 255, 0, 1)",
+                borderWidth: 2,
+              },
+            ],
+          };
+          this.barChartOptions = {
+            responsive: true,
+            legend: {
+              display: false,
+            },
+            title: {
+              display: false
+            },
+            tooltips: {
+              backgroundColor: "#17BF62",
+            },
+            scales: {
+              xAxes: [
+                {
+                  gridLines: {
+                    display: true,
+                  },
+                },
+              ],
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true,
+                    max: 7,
+                    min: 0,
+                    stepSize: 1,
+                  },
+                  gridLines: {
+                    display: true,
+                  },
+                },
+              ],
+            },
+          };
+        }
+        
       }
     }
   }
